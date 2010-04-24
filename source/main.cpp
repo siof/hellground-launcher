4bd349b4e59c7528cd5da86b6238b8bcaff25eed
siof <siof@siof.pl>
2010-04-25 00:14:36 +0200

    execute, checkbox w linuxie, projekt Code::Blocks

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,6 @@
 
 #include <wx/wx.h>
+#include <wx/app.h>
 #include "mainFrame.h"
 
 class LauncherApp: public wxApp
--- source/mainFrame.cpp
+++ source/mainFrame.cpp
@@ -29,7 +29,7 @@ void init_cheat_list()
 {
     // tu komendy pod rozne systemy
     cmd.Add(wxString("tasklist"));
-    cmd.Add(wxString("ps -A"));
+    cmd.Add(wxString("ps"));
     // tu dodajemy wpisy ktore ma sprawdzic
     cheat_list.Add(wxString("wowemuhack"));
     cheat_list.Add(wxString("wpe"));
@@ -44,8 +44,7 @@ bool scan(wxString &string_to_scan)
     return false;
 }
 
-
-bool MainFrame::win_process_scan()
+bool MainFrame::process_scan()
 {
 #ifdef WIN32
     wxString command = cmd[CMD_WINDOWS];
@@ -54,7 +53,7 @@ bool MainFrame::win_process_scan()
 #endif
 
     wxArrayString output, error;
-    wxExecute(command, output, error);
+    wxExecute(command, output, error, wxEXEC_NOEVENTS);
 
     for (wxArrayString::iterator iter = output.begin(); iter != output.end(); ++iter)
         if (scan(*iter))
@@ -67,10 +66,14 @@ MainFrame::MainFrame(const wxString& title)
           wxSYSTEM_MENU | wxCAPTION | wxCLOSE_BOX | wxCLIP_CHILDREN | wxFRAME_SHAPED)
 {
     Center();
+    wxInitAllImageHandlers();
     m_panel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(MAIN_FRAME_WIDTH, MAIN_FRAME_HEIGHT));
 
     m_html = new wxHtmlWindow(this, ID_HTML, wxPoint(5,5), wxSize(MAIN_FRAME_WIDTH-140, MAIN_FRAME_HEIGHT-65), wxHW_SCROLLBAR_AUTO, "WINDOW");
     // nie dziaua load page ;<
+
+    m_html->LoadPage("http://wow.gamefreedom.pl");
+
     m_button[BUTTON_HOME] = new wxButton(m_panel, ID_HOME, wxString("HG Home"),
                                wxPoint(MAIN_FRAME_WIDTH-130, 10), wxSize(120, 60));
 
@@ -89,9 +92,16 @@ MainFrame::MainFrame(const wxString& title)
     m_button[BUTTON_PLAY] = new wxButton(m_panel, ID_PLAY, wxString("Play WoW!"),
                                wxPoint(MAIN_FRAME_WIDTH-130, 360), wxSize(120, 60));
 
+#ifdef LINUX
+    m_checkbox = new wxCheckBox(m_panel, ID_CHECK_BOX, wxString("-opengl"), wxPoint(MAIN_FRAME_WIDTH-100, 425));
+    //ustaw checkboxa zaznaczonego domyslnie
+    m_checkbox->SetValue(true);
+#endif
+
     init_cheat_list();
 
-    bool noob = win_process_scan();
+    //bool noob = win_process_scan();
+    bool noob = lin_process_scan();
 
     // nie wiem jaka to flaga 65536 w define, ale usuwamy domyslna 16 | 65536 bo brzydko wygldala
     CreateStatusBar(1, 65536);
@@ -105,11 +115,24 @@ MainFrame::~MainFrame()
         delete m_button[i];
     delete m_html;
     delete m_panel;
+    //delete m_checkbox; //komentarz bo crashuje ;p
 }
 
 void MainFrame::OnPlay(wxCommandEvent &)
 {
-    wxString cmd("WoW.exe");
+    wxString cmd;
+#ifdef WIN32
+    cmd = "Wow.exe";
+#else
+#ifdef LINUX
+    if (m_checkbox->IsChecked())
+        cmd = "wine WoW.exe -opengl";
+    else
+        cmd = "wine WoW.exe";
+#else
+    //cmd = ; //kod dla maca
+#endif
+#endif
 
     SetStatusText(wxString("Launching World of Warcraft. Please wait..."));
     wxProcess *process = new wxProcess(this, ID_WOW_PROCESS);
