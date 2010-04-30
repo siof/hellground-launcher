72d9e715d8f269e87e270ee5fd5b6befbd73e713
siof <siof@localhost>
2010-04-30 18:56:58 +0200

    znowu zapomniany plik -.-

---
+++ source/grafiki.h
@@ -0,0 +1,13 @@
+#ifndef GRAFIKI_H_INCLUDED
+#define GRAFIKI_H_INCLUDED
+
+#include "../grafika/btnArmory.h"
+#include "../grafika/btnForum.h"
+#include "../grafika/btnHome.h"
+#include "../grafika/btnPanel.h"
+#include "../grafika/btnPlay.h"
+#include "../grafika/htmlBox.h"
+#include "../grafika/tlo.h"
+#include "../grafika/icon.h"
+
+#endif // GRAFIKI_H_INCLUDED
